77842e25be6462d0cb2a6bdb2d2d0289ee141fbd
carlosign <carloscel129@gmail.com>
2023-06-09 18:54:38 -0300

    mercadopago

--- KioscoDeCarlitos/views.py
+++ KioscoDeCarlitos/views.py
@@ -10,6 +10,8 @@ from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 from .serializers import ProductoSerializer
 from .serializers import CategoriaSerializer
 from rest_framework import viewsets
+import mercadopago
+import json
 
 
 class LoginView(APIView):
@@ -79,4 +81,36 @@ class ListarUsuarios(generics.ListCreateAPIView):
         if self.request.user.is_authenticated:
             return Response(serializer.data)
 
+class ProcessPaymentAPIView(APIView):
+    def post(self, request):
+        try:
+            request_values = json.loads(request.body)
+            payment_data = {
+                "transaction_amount": float(request_values["transaction_amount"]),
+                "token": request_values["token"],
+                "installments": int(request_values["installments"]),
+                "payment_method_id": request_values["payment_method_id"],
+                "issuer_id": request_values["issuer_id"],
+                "payer": {
+                    "email": request_values["payer"]["email"],
+                    "identification": {
+                        "type": request_values["payer"]["identification"]["type"],
+                        "number": request_values["payer"]["identification"]["number"],
+                    },
+                },
+            }
 
+            sdk = mercadopago.SDK(str(settings.YOUR_ACCESS_TOKEN))
+
+            payment_response = sdk.payment().create(payment_data)
+
+            payment = payment_response["response"]
+            status = {
+                "id": payment["id"],
+                "status": payment["status"],
+                "status_detail": payment["status_detail"],
+            }
+
+            return Response(data={"body": status, "statusCode": payment_response["status"]}, status=201)
+        except Exception as e:
+            return Response(data={"body": payment_response}, status=400)
